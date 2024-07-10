a6f9ecb10f7c840cfd37cd0bcbc2213ac4f78c91
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-07-10 12:57:52 +0300

    added dev-data & project template

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
+const multer = require("multer");
 const viewsRouter = require("./routes/viewsRoutes");
 const messagesRouter = require("./routes/messagesRoutes");
 
@@ -21,6 +22,18 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use("/", viewsRouter);
 app.use("/", messagesRouter);
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "dev-data/images");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const fileExtension = file.originalname.split(".").pop();
+    cb(null, `${file.fieldname}-${uniqueSuffix}.${fileExtension}`);
+  },
+});
+const upload = multer({ storage: storage });
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
