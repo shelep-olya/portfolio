55506a7a9722e2193bf76f13f7e4f2c9edca8ed0
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-07-10 08:27:47 +0300

    db connected

---
+++ controllers/viewsController.js
@@ -0,0 +1,3 @@
+exports.getMainPage = (req, res) => {
+  res.status(200).render("index");
+};
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
-
-const expressLayout = require("express-ejs-layouts");
+// const expressLayout = require("express-ejs-layouts");
+const viewsRouter = require("./routes/viewsRoutes");
 
 const app = express();
 const PORT = 3000;
@@ -12,8 +13,15 @@ const DB = process.env.MONGODB_URI;
 mongoose.connect(DB).then(() => {
   console.log("DB connected successfully");
 });
-app.listen(PORT, () => {
-  console.log(`App is running on port ${3000}`);
-});
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static("public"));
+// app.use(expressLayout);
 app.set("view engine", "ejs");
+app.use("/", viewsRouter);
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
